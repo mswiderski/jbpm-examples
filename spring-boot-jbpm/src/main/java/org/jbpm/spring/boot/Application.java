12130b4251ac3f25a9db20a9aea9476cf0d1a350
Maciej Swiderski <mswiders@redhat.com>
2015-07-01 17:40:53 +0200

    spring boot example fixed and upgraded to 6.2.0.Final

--- spring-boot-jbpm/src/main/java/org/jbpm/spring/boot/Application.java
+++ spring-boot-jbpm/src/main/java/org/jbpm/spring/boot/Application.java
@@ -4,6 +4,7 @@ import org.jbpm.kie.services.impl.KModuleDeploymentUnit;
 import org.jbpm.services.api.DeploymentService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -11,7 +12,7 @@ import org.springframework.context.annotation.ImportResource;
 
 @Configuration
 @ComponentScan
-@EnableAutoConfiguration
+@EnableAutoConfiguration(exclude=HibernateJpaAutoConfiguration.class)
 @ImportResource(value= {"classpath:config/jee-tx-context.xml",
 		"classpath:config/jpa-context.xml", "classpath:config/jbpm-context.xml", "classpath:config/security-context.xml",})
 public class Application {
